1a9d19c4dcca48d6862d0a7b310f3682fb3a1469
Fernando López Martínez <fer.lopez94@outlook.com>
2018-03-06 02:17:20 -0600

    Add basic login and signup

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const { Model } = require('objection');
 const Knex = require('knex');
 
@@ -18,6 +19,8 @@ class User extends Model {
     }
 }
 
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     res.send({
         name: 'Test',
@@ -25,28 +28,39 @@ app.get('/', (req, res) => {
     });   
 });
 
-app.get('/user', (req, res) => {
-    User.query().then(users => {
-        console.log('Succes:', users);
-        res.send({
-            data: users
+app.get('/user/:email', (req, res) => {
+    const email = req.params.email;
+
+    if (email == undefined) {
+        res.send(400);
+    } else {
+        User.query().where('email', email).then(user => {
+            console.log('Succes:', user);
+            res.send({
+                data: user
+            });
+        }).catch(error => {
+            console.log('Error:', error);
+            res.send(404);
         });
-    }).catch(error => {
-        console.log('Error:', error);
-        res.send({
-            error: error
-        });
-    });
+    }
 });
 
 app.post('/user', (req, res) => {
-    User.query().insert({ first_last_name: 'John', second_last_name: 'Doe'}).then(data => {
-        console.log('Succes:', data);
-        res.send(201);
-    }).catch(error => {
-        console.log('Error:', error);
+    const first_name = req.body.firstName;
+    const email = req.body.email;
+
+    if (first_name == undefined || email == undefined) {
         res.send(400);
-    });
+    } else {
+        User.query().insert({ first_name, email}).then(data => {
+            console.log('Succes:', data);
+            res.send(201);
+        }).catch(error => {
+            console.log('Error:', error);
+            res.send(400);
+        });
+    }
 });
 
 app.listen(port, () => console.log(`Server is up on port ${port}`));
